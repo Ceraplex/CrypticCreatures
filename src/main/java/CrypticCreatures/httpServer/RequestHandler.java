00336e88781e2593762fa4e475363073687dc9ac
Ceraplex <lino-breuss@gmx.at>
2024-10-04 13:19:53 +0200

    added basic Server and RequestHandler code

--- src/main/java/CrypticCreatures/httpServer/RequestHandler.java
+++ src/main/java/CrypticCreatures/httpServer/RequestHandler.java
@@ -1,4 +1,33 @@
 package CrypticCreatures.httpServer;
 
-public class RequestHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class RequestHandler implements Runnable {
+
+    private final Socket clientSocket;
+    private BufferedReader in;
+
+    public RequestHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        // Anfrage lesen, Request verarbeiten und Antwort zurücksenden
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null && !inputLine.isEmpty()){
+                builder.append(inputLine).append(System.lineSeparator());
+            }
+            // TODO process properly
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
--- src/main/java/CrypticCreatures/httpServer/Server.java
+++ src/main/java/CrypticCreatures/httpServer/Server.java
@@ -1,4 +1,23 @@
 package CrypticCreatures.httpServer;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+    public void start(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                threadPool.submit(new RequestHandler(clientSocket));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
