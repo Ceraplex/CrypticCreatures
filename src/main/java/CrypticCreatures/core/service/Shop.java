25d9e3025a6fa4fb369b711e635dcb853b36bd2a
Ceraplex <lino-breuss@gmx.at>
2024-10-06 20:45:57 +0200

    reorganized User class

--- src/main/java/CrypticCreatures/core/service/Shop.java
+++ src/main/java/CrypticCreatures/core/service/Shop.java
@@ -1,8 +1,12 @@
 package CrypticCreatures.core.service;
 
 import CrypticCreatures.core.models.cards.Package;
+
+import java.util.List;
+
 //buy Packages, sell Cards, trade Cards
 public class Shop {
+
     public static Package generatePackage () {
 
         return new Package();
